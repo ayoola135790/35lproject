bb35a87861381ee8c7039b30d1ebefe75e98fce6
ayoola135790 <ayoolaogunleye2004@gmail.com>
2025-03-10 17:07:21 -0700

    testing login, linked to <route>

--- T1D-BloodSugar-App/frontend/pages/HomePage.jsx
+++ T1D-BloodSugar-App/frontend/pages/HomePage.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom"; // Import Link for navigation
 
-function HomePage()
-{
-    return (<>
-        <h1>Home</h1>
-        <p>sup</p>
-    </>)
+function HomePage() {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>sup</p>
+
+      {/* Clickable text that routes to the SignUp page */}
+      <Link to="/login">
+        <p style={{ color: "blue", cursor: "pointer", textDecoration: "underline" }}>
+          Route
+        </p>
+      </Link>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
