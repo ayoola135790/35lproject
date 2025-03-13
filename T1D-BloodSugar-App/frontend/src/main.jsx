118d63d01150481edc90e5c91a37716b4ddffd0e
Emanuel Yilma <emanuelyilma@Emanuels-MacBook-Air.local>
2025-03-12 18:10:51 -0700

    Update main.jsx to include new pages

--- T1D-BloodSugar-App/frontend/src/main.jsx
+++ T1D-BloodSugar-App/frontend/src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import LoginPage from '../pages/LoginPage'
-import WelcomePage from '../pages/WelcomePage'
-import SignupPage from '../pages/SignupPage'
-import GraphPage from '../pages/GraphPage'
-import JournalPage from '../pages/JournalPage'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from '../pages/LoginPage';
+import WelcomePage from '../pages/WelcomePage';
+import SignupPage from '../pages/SignupPage';
+import GraphPage from '../pages/GraphPage';
+import JournalPage from '../pages/JournalPage';
+import ForgotPasswordPage from '../pages/ForgotPasswordPage'; // Import ForgotPasswordPage
+import NewPasswordPage from '../pages/NewPasswordPage'; // Import NewPasswordPage
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -16,7 +18,9 @@ createRoot(document.getElementById('root')).render(
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignupPage />} />
         <Route path='/journal' element={<JournalPage />} />
+        <Route path='/forgot-password' element={<ForgotPasswordPage />} /> {/* Forgot Password Page */}
+        <Route path='/new-password' element={<NewPasswordPage />} /> {/* New Password Page */}
       </Routes>
     </BrowserRouter>
   </StrictMode>,
-)
+);
